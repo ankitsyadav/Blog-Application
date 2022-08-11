51d61df3fa5fcfe9a4e2977d24918d6746dcbe64
ANKIT SINGH YADAV <95612855+ankitsyadav@users.noreply.github.com>
2022-08-12 04:26:19 +0530

    created Spring boot starter project
    laoded basic dependencies created project

---
+++ Personal_Blog_APIs_App/src/main/java/com/blog/PersonalBlogApIsAppApplication.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogApIsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBlogApIsAppApplication.class, args);
+	}
+
+}
