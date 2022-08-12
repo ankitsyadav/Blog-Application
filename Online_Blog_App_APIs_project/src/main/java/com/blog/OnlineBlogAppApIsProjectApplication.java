b4bbee17146a12707513895b13d627d25ef5dbd0
ANKIT SINGH YADAV <95612855+ankitsyadav@users.noreply.github.com>
2022-08-13 02:58:38 +0530

    update

--- Online_Blog_App_APIs_project/src/main/java/com/blog/OnlineBlogAppApIsProjectApplication.java
+++ Online_Blog_App_APIs_project/src/main/java/com/blog/OnlineBlogAppApIsProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogAppApiProjectApplication {
+public class OnlineBlogAppApIsProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogAppApiProjectApplication.class, args);
+		SpringApplication.run(OnlineBlogAppApIsProjectApplication.class, args);
 	}
 
 }
--- Online_Blog_App_APIs_project/src/test/java/com/blog/OnlineBlogAppApIsProjectApplicationTests.java
+++ Online_Blog_App_APIs_project/src/test/java/com/blog/OnlineBlogAppApIsProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogAppApiProjectApplicationTests {
+class OnlineBlogAppApIsProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
